8c16a69dddfa5eb67dd19c53b2ba64f79c29703b
Alexey Arendarenko <alexey.arendarenko@gmail.com>
2020-02-26 06:57:24 +0200

    Create module backend area
    - add helper
    - copyright refactor

---
+++ Helper/Data.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © Alexey Arendarenko, 2020.
+ * https://github.com/arendarenko
+ */
+
+declare(strict_types=1);
+
+namespace Arendarenko\CustomCatalog\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class Data
+ * @package Arendarenko\CustomCatalog\Helper
+ */
+class Data extends AbstractHelper
+{
+    private const MODULE_CONFIG_PATH = 'arendarenko_custom_catalog/';
+
+    /**
+     * @param null $storeId
+     * @return bool
+     */
+    public function isEnabled($storeId = null): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::MODULE_CONFIG_PATH . 'general/is_enabled',
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+}
